8eb037897fa775bc7661e9dbd03a96114d404d97
Johnson Didinya <didinkaj@gmail.com>
2023-10-22 00:36:19 +0300

    making the design responsive

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const sideMenu = document.querySelector('aside');
+const menubtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+
+menubtn.addEventListener('click', ()=>{
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click',()=>{
+    sideMenu.style.display = 'none'
+})
+
+
+darkMode.addEventListener('click', ()=>{
+    document.body.classList.toggle("dark-mode-variables")
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active')
+    darkMode.querySelector("span:nth-child(2)").classList.toggle('active')
+})
+
+
 Orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent = `
         <td>${order.productName}</td>
         <td>${order.productNumber}</td>
-        <td>${order.productStatus}</td>
         <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning': 'primary'}">${order.status}</td>
         <td class="primary">Details</td>
     `;

cbe7d84cc9bf087e95d57fb254774b1b65618f32
Johnson Didinya <didinkaj@gmail.com>
2023-10-21 03:19:35 +0300

    sidebar menu design

---
+++ orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'Javascript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '89790',
+        paymentStatus: 'Active',
+        status: 'Active'
+    },
+    {
+        productName: 'Flex-Box  Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Declined'
+    },
+    {
+        productName: 'Java Tutorial',
+        productNumber: '85643',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]

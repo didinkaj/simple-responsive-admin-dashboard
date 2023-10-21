1d5844b0bceb9148177b584beb25848d3806d7ef
Johnson Didinya <didinkaj@gmail.com>
2023-10-21 06:48:07 +0300

    added dashboard main content and right side sections

---
+++ index.js
@@ -0,0 +1,13 @@
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.productStatus}</td>
+        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning': 'primary'}">${order.status}</td>
+        <td class="primary">Details</td>
+    `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+    
+});
